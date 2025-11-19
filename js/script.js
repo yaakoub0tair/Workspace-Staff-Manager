f1170bb4bb18506ca23fe4bda2be249ab230b96d
Yaakoub tair <yaakoubt05@gmail.com>
2025-11-19 22:03:20 +0100

    add :ajouter employer a zone

--- js/script.js
+++ js/script.js
@@ -62,3 +62,40 @@ function displayUnassigned() {
 
     attachAssignButtons();
 }
+
+function attachAssignButtons() {
+    const buttons = document.querySelectorAll('.assign-btn');
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const id = btn.getAttribute('data-id');
+
+            // فتح قائمة اختيار Zone
+            const zone = prompt("Enter zone ID (conference, reception, servers, security, staff, archives):");
+
+            assignToZone(id, zone);
+        });
+    });
+}
+
+function assignToZone(id, zoneId) {
+    const emp = employees.find(e => e.id == id);
+
+    if (!emp) return;
+
+    // Zone restrictions
+    if (!checkAccess(emp.role, zoneId)) {
+        alert("Ce rôle n'a pas accès à cette zone !");
+        return;
+    }
+
+    // Max capacity per zone
+    const zoneElement = document.getElementById(zoneId);
+    if (zoneElement.children.length >= 4) {
+        alert("Zone pleine !");
+        return;
+    }
+
+    emp.location = zoneId;
+    updateZones();
+}
