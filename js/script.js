bc83fcb8cfcae15c75e4931e45058ed954fb2996
Yaakoub tair <yaakoubt05@gmail.com>
2025-11-19 22:04:25 +0100

    feat : eccess zone function

--- js/script.js
+++ js/script.js
@@ -99,3 +99,43 @@ function assignToZone(id, zoneId) {
     emp.location = zoneId;
     updateZones();
 }
+
+
+function checkAccess(role, zone) {
+    const rules = {
+        reception: ["Réceptionniste", "Manager"],
+        servers: ["Technicien", "Manager"],
+        security: ["Agent sécurité", "Manager"],
+        archives: ["Manager"],
+        conference: ["Manager", "Employé", "Technicien", "Staff"],
+        staff: ["Manager", "Employé", "Nettoyage", "Staff"]
+    };
+
+    if (rules[zone] && rules[zone].includes(role)) {
+        return true;
+    }
+    return false;
+}
+
+function updateZones() {
+    const zoneIds = ["conference", "reception", "servers", "security", "staff", "archives"];
+
+    zoneIds.forEach(z => {
+        const container = document.getElementById(z);
+        container.innerHTML = "";
+
+        employees.forEach(emp => {
+            if (emp.location === z) {
+                const div = document.createElement("div");
+                div.classList.add("worker");
+                div.innerHTML = `
+                    ${emp.name}
+                    <br><span class="role">${emp.role}</span>
+                `;
+                container.appendChild(div);
+            }
+        });
+    });
+
+    displayUnassigned();
+}
