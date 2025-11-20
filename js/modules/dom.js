11d1823958b0dc30a5016aade1eccf7ec06f911e
Yaakoub tair <yaakoubt05@gmail.com>
2025-11-20 03:35:20 +0100

    feat : ajouter un fichier dom

---
+++ js/modules/dom.js
@@ -0,0 +1,22 @@
+export const DOM = {
+
+    popupWorker: document.getElementById('popup-worker'),
+    profileModal: document.getElementById('profileModal'),
+    btnClosePopup: document.querySelector('#popup-worker .btn-close'),
+
+
+    formWorker: document.getElementById('form-worker'),
+    btnAddExperience: document.querySelector('.btn-add-experience'),
+    experienceArea: document.querySelector('.experience-area'),
+    firstExperienceItem: document.querySelector('.experience-area .experience-item'),
+
+
+    btnAddWorker: document.getElementById('btn-add-worker'),
+    listWorkers: document.getElementById('list-workers'),
+    searchInput: document.getElementById('searchInput'),
+    filterButtons: document.querySelectorAll('.filter-btn'),
+
+
+    zones: document.querySelectorAll('.zone'),
+    btnAddZone: document.querySelectorAll('.btn-add-zone')
+};
