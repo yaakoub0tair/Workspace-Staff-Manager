a6fd925379e6332b22096136b2fdb9f7163acb8d
Yaakoub tair <yaakoubt05@gmail.com>
2025-11-23 03:27:40 +0100

    feat: Add modal management and event handlers

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,20 +1,36 @@
+// modal.js
 import { DOM } from './dom.js';
 
 export function initModal() {
+    if (!DOM.btnAddWorker || !DOM.popupWorker) {
+        console.error("Erreur : Un ou plusieurs éléments du popup 'Add Worker' sont introuvables.");
+        return;
+    }
+
 
     DOM.btnAddWorker.addEventListener('click', () => {
         DOM.popupWorker.classList.add('active');
     });
 
 
-    DOM.btnClosePopup.addEventListener('click', () => {
-        DOM.popupWorker.classList.remove('active');
-    });
+    if (DOM.btnClosePopup) {
+        DOM.btnClosePopup.addEventListener('click', () => {
+            DOM.popupWorker.classList.remove('active');
+        });
+    }
 
 
-    DOM.popupWorker.addEventListener('click', (e) => {
-        if (e.target === DOM.popupWorker) {
+    DOM.popupWorker.addEventListener('click', (event) => {
+        if (event.target === DOM.popupWorker) {
             DOM.popupWorker.classList.remove('active');
         }
     });
+
+    if (DOM.profileModal) {
+        DOM.profileModal.addEventListener('click', (event) => {
+            if (event.target === DOM.profileModal) {
+                DOM.profileModal.classList.add('hidden');
+            }
+        });
+    }
 }
