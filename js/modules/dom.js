7d464daf3556b3d70d0773e708c544ed2db1c771
Yaakoub tair <yaakoubt05@gmail.com>
2025-11-23 03:25:58 +0100

    chore: Centralize DOM element references

--- js/modules/dom.js
+++ js/modules/dom.js
@@ -1,22 +1,38 @@
 export const DOM = {
 
+    // Popups
     popupWorker: document.getElementById('popup-worker'),
     profileModal: document.getElementById('profileModal'),
     btnClosePopup: document.querySelector('#popup-worker .btn-close'),
 
-
+    // Worker Form
     formWorker: document.getElementById('form-worker'),
-    btnAddExperience: document.querySelector('.btn-add-experience'),
-    experienceArea: document.querySelector('.experience-area'),
-    firstExperienceItem: document.querySelector('.experience-area .experience-item'),
+    workerName: document.getElementById('workerName'),
+    workerRole: document.getElementById('workerRole'),
+    workerPhoto: document.getElementById('workerPhoto'),
+    workerEmail: document.getElementById('workerEmail'),
+    workerPhone: document.getElementById('workerPhone'),
 
+    // Experience
+    btnAddExperience: document.getElementById('btnAddExperience'),
+    experienceArea: document.getElementById('experienceArea'),
+    firstExperienceItem: document.getElementById('firstExperienceItem'),
 
+    // Workers List
     btnAddWorker: document.getElementById('btn-add-worker'),
     listWorkers: document.getElementById('list-workers'),
     searchInput: document.getElementById('searchInput'),
     filterButtons: document.querySelectorAll('.filter-btn'),
 
-
+    // Zones
     zones: document.querySelectorAll('.zone'),
-    btnAddZone: document.querySelectorAll('.btn-add-zone')
+    btnAddZone: document.querySelectorAll('.btn-add-zone'),
+
+    // Selection Modal
+    selectionModal: document.getElementById('worker-selection-modal'),
+    selectionList: document.getElementById('worker-selection-list'),
+    selectionTitle: document.getElementById('selection-modal-title'),
+    noWorkersMessage: document.getElementById('no-workers-message'),
+    btnCloseSelection: document.querySelector('#worker-selection-modal .btn-close-selection'),
+
 };
